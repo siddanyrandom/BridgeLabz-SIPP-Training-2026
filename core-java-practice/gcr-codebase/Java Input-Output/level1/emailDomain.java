f17d89d2b16c15a3842a336f7d3d9ac174d27266
Siddhartha Saxena Kanojia <siddharthasaxena11a@gmail.com>
2026-06-19 23:51:53 +0530

    Create emailDomain.java

---
+++ core-java-practice/gcr-codebase/Java Input-Output/level1/emailDomain.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class EmailDomainCounter {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br =
+                new BufferedReader(new FileReader("emails.txt"));
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        String email;
+
+        while ((email = br.readLine()) != null) {
+
+            String domain =
+                    email.substring(email.indexOf("@") + 1);
+
+            map.put(domain,
+                    map.getOrDefault(domain, 0) + 1);
+        }
+
+        for (String domain : map.keySet()) {
+            System.out.println(domain + " : " + map.get(domain));
+        }
+
+        br.close();
+    }
+}
