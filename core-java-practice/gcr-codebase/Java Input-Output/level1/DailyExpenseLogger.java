07429c61e9a6b912537939ecaeccd62614ae2cc3
Siddhartha Saxena Kanojia <siddharthasaxena11a@gmail.com>
2026-06-19 23:51:26 +0530

    Create DailyExpenseLogger.java

---
+++ core-java-practice/gcr-codebase/Java Input-Output/level1/DailyExpenseLogger.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class CustomerFeedbackAnalyzer {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        int count = 0;
+
+        for (int i = 0; i < 5; i++) {
+            String feedback = br.readLine().toLowerCase();
+
+            if (feedback.contains("good"))
+                count++;
+        }
+
+        System.out.println("Good Feedback Count = " + count);
+    }
+}
