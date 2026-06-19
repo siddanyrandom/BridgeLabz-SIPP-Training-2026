0d576dc4e89af296b3fdaa1d04b8ed91bd649d74
Siddhartha Saxena Kanojia <siddharthasaxena11a@gmail.com>
2026-06-19 23:46:51 +0530

    Create Power.java

---
+++ core-java-practice/gcr-codebase/Java-Recursion/level1/Power.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Power {
+
+    static int power(int x, int n) {
+        if (n == 0)
+            return 1;
+        return x * power(x, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter x: ");
+        int x = sc.nextInt();
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        System.out.println(power(x, n));
+        sc.close();
+    }
+}
