6a4518b19f494aeb246a2990bce7289f2f84afbd
Siddhartha Saxena Kanojia <siddharthasaxena11a@gmail.com>
2026-06-22 12:21:35 +0530

    Add ArrayOperations class with exception handling

---
+++ core-java-practice/gcr-codebase/Java-ExceptionHandling/level-1/ArrayOperations.java
@@ -0,0 +1,1 @@
+public class ArrayOperation { public static void main(String[] args){ Integer[] arr={10,20,30}; try{ System.out.println(arr[5]); } catch(ArrayIndexOutOfBoundsException e){ System.out.println("Invalid index!"); } catch(NullPointerException e){ System.out.println("Array is not initialized!"); } } }
