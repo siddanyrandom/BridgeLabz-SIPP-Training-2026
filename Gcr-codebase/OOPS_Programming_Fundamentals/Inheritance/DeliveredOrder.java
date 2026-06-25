48f4ae69b05b7b51ec630d7a3cec1a4095f9ff82
Siddhartha Saxena Kanojia <siddharthasaxena11a@gmail.com>
2026-06-25 10:12:18 +0530

    Create DeliveredOrder.java

---
+++ Gcr-codebase/OOPS_Programming_Fundamentals/Inheritance/DeliveredOrder.java
@@ -0,0 +1,44 @@
+class Order {
+    int orderId;
+    String orderDate;
+
+    Order(int orderId, String orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+    }
+
+    String getOrderStatus() {
+        return "Order Placed";
+    }
+}
+
+class ShippedOrder extends Order {
+    String trackingNumber;
+
+    ShippedOrder(int orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+
+    String getOrderStatus() {
+        return "Order Shipped";
+    }
+}
+
+public class DeliveredOrder extends ShippedOrder {
+    String deliveryDate;
+
+    DeliveredOrder(int orderId, String orderDate, String trackingNumber, String deliveryDate) {
+        super(orderId, orderDate, trackingNumber);
+        this.deliveryDate = deliveryDate;
+    }
+
+    String getOrderStatus() {
+        return "Order Delivered";
+    }
+
+    public static void main(String[] args) {
+        DeliveredOrder d = new DeliveredOrder(1, "25-06-2026", "TR123", "28-06-2026");
+        System.out.println(d.getOrderStatus());
+    }
+}
