bcbf8e293fd312ff7f8c61348104262510f41408
Siddhartha Saxena Kanojia <siddharthasaxena11a@gmail.com>
2026-06-25 10:15:53 +0530

    Add PaidOnlineCourse class with inheritance structure

---
+++ Gcr-codebase/OOPS_Programming_Fundamentals/Inheritance/restaurant.java
@@ -0,0 +1,36 @@
+class Course {
+    String courseName;
+    int duration;
+
+    Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+}
+
+class OnlineCourse extends Course {
+    String platform;
+    boolean isRecorded;
+
+    OnlineCourse(String courseName, int duration, String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+}
+
+public class PaidOnlineCourse extends OnlineCourse {
+    double fee;
+    double discount;
+
+    PaidOnlineCourse(String courseName, int duration, String platform, boolean isRecorded, double fee, double discount) {
+        super(courseName, duration, platform, isRecorded);
+        this.fee = fee;
+        this.discount = discount;
+    }
+
+    public static void main(String[] args) {
+        PaidOnlineCourse c = new PaidOnlineCourse("Java", 30, "Udemy", true, 5000, 10);
+        System.out.println(c.courseName + " Fee: " + c.fee);
+    }
+}
