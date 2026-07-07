2278c7c6ea16389b84a4bcfb5372e2e522972e08
Siddhartha Saxena Kanojia <siddharthasaxena11a@gmail.com>
2026-07-07 12:13:09 +0530

    Implement merge sort for patient registration IDs

---
+++ SortingScenarioBasedQuestions/PatientRegistrationIDs.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class MergeSortPatientIDs {
+
+    static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    static void merge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            leftArray[i] = arr[left + i];
+
+        for (int j = 0; j < n2; j++)
+            rightArray[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] patientIDs = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            patientIDs[i] = sc.nextInt();
+        }
+
+        mergeSort(patientIDs, 0, n - 1);
+
+        System.out.print("[");
+        for (int i = 0; i < n; i++) {
+            System.out.print(patientIDs[i]);
+            if (i != n - 1)
+                System.out.print(" ");
+        }
+        System.out.println("]");
+
+        sc.close();
+    }
+}
