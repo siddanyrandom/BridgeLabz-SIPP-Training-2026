987aa511b44b3fd59871dcb78433c29ff3fd406a
Siddhartha Saxena Kanojia <siddharthasaxena11a@gmail.com>
2026-07-08 12:33:58 +0530

    Implement methods to find first and last occurrence

---
+++ BinarySearch/FirstandLastelementofansortedArray.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class FirstAndLastOccurrence {
+
+    // Find First Occurrence
+    static int firstOccurrence(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                right = mid - 1;   // Search left half
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    // Find Last Occurrence
+    static int lastOccurrence(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                left = mid + 1;   // Search right half
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        int first = firstOccurrence(arr, target);
+        int last = lastOccurrence(arr, target);
+
+        System.out.println("First Occurrence: " + first);
+        System.out.println("Last Occurrence: " + last);
+
+        sc.close();
+    }
+}
