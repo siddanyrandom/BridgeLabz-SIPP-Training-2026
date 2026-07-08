822ae8a06c43d94ef7cf36ce277c28970e4087d2
Siddhartha Saxena Kanojia <siddharthasaxena11a@gmail.com>
2026-07-08 12:29:48 +0530

    Create PeakElementinanArray.java

---
+++ BinarySearch/PeakElementinanArray.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class FindPeakElement {
+
+    static int findPeak(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;   
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int peakIndex = findPeak(arr);
+
+        System.out.println("Peak Element Index: " + peakIndex);
+        System.out.println("Peak Element: " + arr[peakIndex]);
+
+        sc.close();
+    }
+}
