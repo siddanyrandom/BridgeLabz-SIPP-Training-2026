c2cec80871bf6e38953a121e4f8f06be64a46264
Siddhartha Saxena Kanojia <siddharthasaxena11a@gmail.com>
2026-07-13 10:23:49 +0530

    Add findPairSum method for transaction pairs
    Implement a method to find a pair of transactions that sum to a target value using the two-pointer technique.

---
+++ Two_pointers_Sliding_window_control/Scenario1.java
@@ -0,0 +1,20 @@
+package TwoPointersSlidingWindow;
+
+public class Scenario1 {
+    public static int[] findPairSum(int[] transactions, int target) {
+        int left = 0, right = transactions.length - 1;
+
+        while (left < right) {
+            int sum = transactions[left] + transactions[right];
+
+            if (sum == target)
+                return new int[]{transactions[left], transactions[right]};
+            else if (sum < target)
+                left++;
+            else
+                right--;
+        }
+
+        return new int[]{-1, -1};
+    }
+}
