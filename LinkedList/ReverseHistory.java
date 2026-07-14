87e51edaeb4cc302504a19a635241938a77d486b
Siddhartha Saxena Kanojia <siddharthasaxena11a@gmail.com>
2026-07-14 22:53:49 +0530

    Implement ReverseHistory class to reverse linked list

---
+++ LinkedList/ReverseHistory.java
@@ -0,0 +1,20 @@
+class Node {
+    int val;
+    Node next;
+    Node(int val){
+        this.val=val;
+    }
+}
+
+public class ReverseHistory {
+    static Node reverseHistory(Node head){
+        Node prev=null,curr=head;
+        while(curr!=null){
+            Node next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
