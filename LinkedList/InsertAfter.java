8af8d2ca6b1223e60eca8513bb024904b0d34839
Siddhartha Saxena Kanojia <siddharthasaxena11a@gmail.com>
2026-07-14 22:50:09 +0530

    Implement insertAfter method in InsertAfter class

---
+++ LinkedList/InsertAfter.java
@@ -0,0 +1,16 @@
+class Node {
+    int val;
+    Node next;
+    Node(int val){
+        this.val=val;
+    }
+}
+
+public class InsertAfter {
+    static void insertAfter(Node current, int trackId){
+        if(current==null) return;
+        Node newNode=new Node(trackId);
+        newNode.next=current.next;
+        current.next=newNode;
+    }
+}
